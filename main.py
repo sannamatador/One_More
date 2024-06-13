403b6bbaa93ede42dd86d8436f8a8ecad9575677
sannamatador <sanna.matador@gmail.com>
2024-06-13 20:29:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# Создайте новый проект в PyCharm
+# Запустите созданный проект
+# Ваша задача:
+# Создайте новую функцию def test_function
+# Создайте другую функцию внутри функции inner_function, функция должна печатать значение "Я в области видимости функции test_function"
+# Вызовите функцию inner_function внутри функции test_function
+# Попробуйте вызывать inner_function вне функции test_function и посмотрите на результат выполнения программы
+# Полученный код напишите в ответ к домашнему заданию
+#
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+    inner_function()
+
+test_function()
