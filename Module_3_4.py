c84b9bf13542868a13988802f1d5cbe8c3248992
sannamatador <sanna.matador@gmail.com>
2024-11-15 16:51:15 +0300

    Add files via upload

---
+++ Module_3_4.py
@@ -0,0 +1,19 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+    root_word_lower = root_word.lower()
+    for word in other_words:
+        word_lower = word.lower()
+        if root_word_lower in word_lower or word_lower in root_word_lower:
+            same_words.append(word)  
+    return same_words
+
+
+# Примеры вызовов функции
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+
+# Выводим результаты
+print(result1)  # ['richiest', 'orichalcum', 'richies']
+print(result2)  # ['Able', 'Disable']
+
+
